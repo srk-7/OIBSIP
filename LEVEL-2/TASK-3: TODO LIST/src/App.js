48c47c6b1cf8f76852279277ff1b1a25339c65bc
ARAVAPALLY SHIVARAMAKRISHNA <105867469+srk-7@users.noreply.github.com>
2022-12-10 18:35:36 +0530

    Add files via upload

--- LEVEL-2/TASK-3: TODO LIST/src/App.js
+++ LEVEL-2/TASK-3: TODO LIST/src/App.js
@@ -1,1 +1,13 @@
+import TodoList from './components/TodoList';
+import './App.css';
 
+function App() {
+  return (
+    <div className='todo-list-container'>
+      <TodoList/>
+      
+    </div>
+  );
+}
+
+export default App;
